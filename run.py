e079f98e2fb7813bd3da82e88bd7c2c6ea2ca4fd
serbelko <ser.belko12@gmail.com>
2025-03-14 12:47:45 +0300

    build: first tg bot command

--- run.py
+++ run.py
@@ -1,1 +1,29 @@
-# hello world
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from dotenv import load_dotenv
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def echo(message: Message) -> None:
+    await message.answer("Привет")
+
+
+async def main() -> None:
+    # переименуй файл .env.dist в .env и подставь соотвествующие данные
+    load_dotenv()
+    bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+    
+    dp.message.register(echo, F.text)
+
+    bot = Bot(token=bot_token)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
